7df0bcb5dbfff50f25e42082c15fcd8cca9f406f
So1var <21rikoto@wqaipahuh.k12.hi.us>
2020-02-07 14:28:48 -1000

    Third Commit

--- tip-gen.js
+++ tip-gen.js
@@ -1,5 +1,25 @@
-// 15 - 20% tip average 18.9%
+// 15 - 20% tip, average is 18-19%
 
-function calctip(us){
-  us / 10;
+
+
+/*function calctip(){
+var t = document.getElementsByName(x)
+  tip = t * p
+  document.getElementById('tip').innerHTML =
+}*/
+
+
+
+function realnumx() {
+	var x = document.getElementsByTagName('x');
+    Math.round(100 * x) / 100; //nearest 100th
+    document.getElementById('x').innerHTML = x;
+    var t = setTimeout(realnumx,500);
+}
+
+function realnumy(){
+	var y = document.getElementsByTagName('y');
+    Math.round(100 * y) / 100; //nearest 100th
+    document.getElementById('y').innerHTML = y;
+    var m = setTimeout(realnumy,500);
 }
